4c8cfa4cf8f0f6dd8e57c08cb5a3116e03edfeb5
davidokun <davidokun99@gmail.com>
2018-03-26 16:21:12 -0500

    Adding build script for docker images

---
+++ build-docker.sh
@@ -0,0 +1,24 @@
+# bin/bash
+echo ---------------------------- &&
+echo -- Building Config-Server -- &&
+echo ---------------------------- &&
+cd config-server/docker && docker build -t config-server . &&
+echo ---------------------------- &&
+echo -- Building Game-Service  -- &&
+echo ---------------------------- &&
+cd ../../game-service/docker && docker build -t game-service . &&
+echo ----------------------------- &&
+echo -- Building Gateway-Server -- &&
+echo ----------------------------- &&
+cd ../../gateway-service/docker && docker build -t gateway-server . &&
+echo -------------------------------- &&
+echo -- Building Publisher-Service -- &&
+echo -------------------------------- &&
+cd ../../publisher-service/docker && docker build -t publishe-service . &&
+echo ------------------------------ &&
+echo -- Building Registry-Server -- &&
+echo ------------------------------ &&
+cd ../../registry-server/docker && docker build -t registry-server . &&
+echo -------------- &&
+echo -- Finished -- &&
+echo --------------
